69e7a7f5ea0ad0018c4752c8f98c4ec1a4fd871c
Ruslan Zhalnin <zhrv@mrsu.ru>
2018-10-19 08:17:03 +0300

    bnd condition fixed

--- src/charm_bnd_cond.c
+++ src/charm_bnd_cond.c
@@ -26,7 +26,9 @@ void charm_bnd_cond(p4est_t* p4est, p4est_topidx_t treeid, int8_t face,
     CHARM_ASSERT(bnd);
     bnd->bnd_fn(par_in, par_out, face, bnd->params, n);
     par_out->mat_id = par_in->mat_id;
-    mat->eos_fn(p4est, par_out, 3); // (T,p) => (r, cz, e)
+    if (bnd->type == BOUND_INLET) { // @todo
+        mat->eos_fn(p4est, par_out, 3); // (T,p) => (r, cz, e)
+    }
     par_out->e_tot = par_out->e+0.5*_MAG_(par_out->u, par_out->v, par_out->w);
 }
 
