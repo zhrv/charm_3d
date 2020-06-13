a50775864d31770a16e941fd1ba434bf7581da89
Ruslan Zhalnin <zhalnin@gmail.com>
2020-06-13 14:13:21 +0300

    Version checking is added

---
+++ include/charm_version.h
@@ -0,0 +1,11 @@
+//
+// Created by zhrv on 13.06.2020.
+//
+
+#ifndef CHARM_3D_CHARM_VERSION_H
+#define CHARM_3D_CHARM_VERSION_H
+
+static const char CHARM_VERSION[]   = "0.0.3";
+static const char YAML_VERSION[]    = "0.1.1";
+
+#endif //CHARM_3D_CHARM_VERSION_H
